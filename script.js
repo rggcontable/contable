6b1ee12b41910361faa8535d93ab2da3803e2683
rggcontable <rggcontable@gmail.com>
2024-12-11 14:37:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('clickButton').addEventListener('click', function () {
+    const message = document.getElementById('message');
+    message.textContent = '¡Gracias por hacer clic! Aprende más sobre contabilidad.';
+    message.style.color = 'green';
+});
